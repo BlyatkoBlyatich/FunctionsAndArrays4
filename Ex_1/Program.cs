d3bfa623a5e29b436ceabedb254edb746dc0d8b5
Василий Виссаров <vissar2016@rambler.ru>
2022-11-12 02:59:37 +0300

    Ex 1 is done

---
+++ Ex_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Pow(int num1, int num2)
+{
+    int result = 1;
+    for(int i = 0; i < num2; i++)
+    {
+        result *= num1;
+    }
+    return result;
+}
+
+Console.WriteLine("Input first number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inpur second number");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Stpyen num1 in num2 = {Pow(a, b)}");
