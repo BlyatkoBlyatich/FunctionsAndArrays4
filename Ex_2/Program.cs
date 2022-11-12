8c6c4de09b5a684038706d2832c561561aec6f5f
Василий Виссаров <vissar2016@rambler.ru>
2022-11-12 03:00:10 +0300

    Ex 2 is done

---
+++ Ex_2/Program.cs
@@ -0,0 +1,17 @@
+﻿ //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumOfNum(int number)
+{
+    int sum = 0;
+    while(number != 0)
+    {
+        sum = sum + number%10; // присваем значение sum последнее цифру вводимого числа
+        number = number/10; // избавляемся от последней цифры введенного числа и переходим к циклу, пока число не равно 0
+    }
+    return sum; // возвращаем полученное значение sum и завершаем метод
+}
+
+Console.WriteLine("Input a number"); // вводим число
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($" Sum of digit number is: {SumOfNum(num)}"); // выводим получение значение, рез
